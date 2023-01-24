486ac996323bc9e92d08d9572f7873b46d0baa7b
Brent <jbmsharpe@gmail.com>
2023-01-24 13:01:38 -0500

    created pages for the cart and shop, created array of prodcuts, created display of products on shop page

---
+++ src/products.js
@@ -0,0 +1,34 @@
+// Find your own images and put them in the assets folder
+
+import product1 from './assets/autograph.png'
+import product2 from './assets/autograph.png'
+import product3 from './assets/autograph.png'
+import product4 from './assets/autograph.png'
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'Ken Griffey Jr. Autograph',
+        price: 1499.00,
+        productImage: product1
+    },
+    {
+        id: 1,
+        productName: 'Ken Griffey Jr. Autograph',
+        price: 1499.00,
+        productImage: product2
+    },
+    {
+        id: 1,
+        productName: 'Ken Griffey Jr. Autograph',
+        price: 1499.00,
+        productImage: product3
+    },
+    {
+        id: 1,
+        productName: 'Ken Griffey Jr. Autograph',
+        price: 1499.00,
+        productImage: product4
+    }
+]
