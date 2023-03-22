1defdc13732f9da983ee8ee31b8501aee162aa84
Brent <jbmsharpe@gmail.com>
2023-03-22 11:56:24 -0400

    gave functionality to the number of each item in the cart, created buttons for continue shopping and checkout, gave cart a subtotal that adds up total amount of cart

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,5 +1,6 @@
 import React, {createContext} from 'react';
 import { PRODUCTS } from '../products';
+import {useState} from 'react';
 
 
 export const ShopContext = createContext(null);
@@ -7,7 +8,7 @@ export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
     let cart = {}
-    for (let i =1; i < PRODUCTS.length; i++) {
+    for (let i =1; i <= PRODUCTS.length; i++) {
         cart[i] = 0;
     }
     return cart;
@@ -17,9 +18,34 @@ const getDefaultCart = () => {
 export const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if(cartItems[item] > 0) {
+                let itemInfo = PRODUCTS.find((product) => product.id ===Number(item));
+                totalAmount += cartItems[item] * itemInfo.price;
+            }
+        }
+
+        return totalAmount
+    };
+
     const addToCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1 }));
     };
-    return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] -1 }));
+    };
+    
+    const updateCartItemCount = (newAmount, itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: newAmount }))
+    } 
+
+    const contextValue = {cartItems, addToCart, removeFromCart, updateCartItemCount, getTotalCartAmount};
+
+    return (<ShopContext.Provider value={contextValue}>
+        {props.children}
+        </ShopContext.Provider>);
   
 };
--- src/products.js
+++ src/products.js
@@ -1,9 +1,9 @@
 // Find your own images and put them in the assets folder
 
 import product1 from './assets/autograph.png'
-import product2 from './assets/autograph.png'
-import product3 from './assets/autograph.png'
-import product4 from './assets/autograph.png'
+import product2 from './assets/guitar.png'
+import product3 from './assets/oven.png'
+import product4 from './assets/italy.png'
 
 
 export const PRODUCTS = [
@@ -14,21 +14,21 @@ export const PRODUCTS = [
         productImage: product1
     },
     {
-        id: 1,
-        productName: 'Ken Griffey Jr. Autograph',
-        price: 1499.00,
+        id: 2,
+        productName: 'Gibson Les Paul',
+        price: 2399.00,
         productImage: product2
     },
     {
-        id: 1,
-        productName: 'Ken Griffey Jr. Autograph',
-        price: 1499.00,
+        id: 3,
+        productName: 'Ooni Karu 12 Pizza Oven',
+        price: 599.00,
         productImage: product3
     },
     {
-        id: 1,
-        productName: 'Ken Griffey Jr. Autograph',
-        price: 1499.00,
+        id: 4,
+        productName: 'Trip to Italy for 2',
+        price: 849.00,
         productImage: product4
-    }
+    },
 ]
